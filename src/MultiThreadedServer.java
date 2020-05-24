73d4ec479f24d343a74ab57357fd0aff29bf5123
seniorturkmen <mustafatrkmn@yandex.com>
2020-05-24 16:01:00 +0300

    MultithreadedServer Side re named

--- src/MultiThreadedServer.java
+++ src/MultiThreadedServer.java
@@ -1,26 +1,27 @@
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class MultiThreadSocketServer {
+public class MultiThreadedServer {
     public static void main(String[] args) throws Exception {
         try{
             ServerSocket server =   new ServerSocket(4027);
             int counter         =   0;
-        System.out.println("Server Started ....");
+            System.out.println("Server Started ....");
             while(true){
                 counter++;
 
-                //server accept the client connection request
+                //  server accept the client connection request
                 Socket serverClient     =   server.accept();
 
                 System.out.println(" >> " + "Client No:" + counter + " started!");
 
-                //send  the request to a separate thread
-                ServerClientThread sct  =   new ServerClientThread(serverClient, counter);
+                //  send  the request to a separate thread
+                ServerThread sct  =   new ServerThread(serverClient, counter);
 
                 sct.start();
             }
         }catch(Exception e){
+            System.out.println("Multi thread Server Ex...");
             System.out.println(e);
         }
     }
